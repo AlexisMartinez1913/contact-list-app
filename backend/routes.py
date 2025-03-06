8e21ac125b4bd1f2df1927420602859a52430fc7
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-05 21:04:31 -0500

    create route to create friend

--- backend/routes.py
+++ backend/routes.py
@@ -10,3 +10,33 @@ def get_friend():
     #crear una lista en formato JSON
     result = [friend.to_json() for friend in friends]
     return jsonify(result)
+
+#creae a friend
+@app.route('/api/friends', methods=["POST"])
+def create_friend():
+    try:
+        data = request.get_json()
+        
+        name = data.get("name")
+        role = data.get("role")
+        description = data.get("description")
+        gender = data.get("gender")
+        
+        #Fetch avatar imagen basado en el genero
+        if gender == "male":
+            img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+        elif gender == "female":
+            img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+        else:
+            img_url = None
+        
+        new_friend = Friend(name=name, role=role, description=description, gender=gender, img_url=img_url)
+        
+        db.session.add(new_friend)
+        db.session.commit()
+        
+        return jsonify({"msg": "Friend created successfully"}), 201
+    
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
